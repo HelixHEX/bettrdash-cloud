11ed09bf1ff387045607730907bcbf6b28785847
Elias <e.wambugu192@gmail.com>
2023-04-18 00:56:12 -0700

    testing api vercel route

--- apps/api-test/index.js
+++ apps/api-test/index.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const app = express();
-const product = require("./api/product");
+const product = require("./product");
 
 app.use(express.json({ extended: false }));
 
-app.use("/api/product", product);
+app.use("/product", product);
 
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => console.log(`Server is running in port ${PORT}`));

869a23d0490eeec1e91c8acd6e26c284d215a0d6
Elias <e.wambugu192@gmail.com>
2023-04-18 10:57:04 -0700

    reconfigured eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,22 +1,11 @@
-/** @type {import("eslint").Linter.Config} */
-const config = {
+
+module.exports = {
   root: true,
-  extends: ["eslint-config-custom"], // uses the config in `packages/config/eslint`
-  parser: "@typescript-eslint/parser",
-  parserOptions: {
-    ecmaVersion: "latest",
-    tsconfigRootDir: __dirname,
-    project: [
-      "./tsconfig.json",
-      "./apps/*/tsconfig.json",
-      "./packages/*/tsconfig.json",
-    ],
-  },
+  // This tells ESLint to load the config from the package `eslint-config-custom`
+  extends: ["custom"],
   settings: {
     next: {
-      rootDir: ["apps/nextjs"],
+      rootDir: ["apps/*/"],
     },
-  },
+  }
 };
-
-module.exports = config;

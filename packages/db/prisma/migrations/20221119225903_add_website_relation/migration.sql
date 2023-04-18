91f8dd38f9ccbfca50ec9664167797000f82570a
Elias <e.wambugu192@gmail.com>
2023-04-18 10:19:18 -0700

    removed prisma migration

--- packages/db/prisma/migrations/20221119225903_add_website_relation/migration.sql
+++
@@ -1,8 +0,0 @@
--- DropForeignKey
-ALTER TABLE "Website" DROP CONSTRAINT "Website_projectId_fkey";
-
--- AlterTable
-ALTER TABLE "Website" ALTER COLUMN "projectId" DROP NOT NULL;
-
--- AddForeignKey
-ALTER TABLE "Website" ADD CONSTRAINT "Website_projectId_fkey" FOREIGN KEY ("projectId") REFERENCES "Project"("id") ON DELETE SET NULL ON UPDATE CASCADE;
--- packages/db/prisma/migrations/20221216030540_added_default_website_tag/migration.sql
+++
@@ -1,5 +0,0 @@
--- AlterTable
-ALTER TABLE "Project" ADD COLUMN     "defaultWebsiteId" INTEGER;
-
--- AlterTable
-ALTER TABLE "Website" ADD COLUMN     "default" BOOLEAN NOT NULL DEFAULT false;

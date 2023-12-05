8b2ee901791007fc837134be0a58054a564f5f32
Elias Wambugu <e.wambugu192@gmail.com>
2023-12-04 17:31:17 -0800

    updated monitor api to vercel serverless function

--- apps/monitor/api/index.ts
+++ apps/monitor/api/index.ts
@@ -1,9 +1,74 @@
+import type { VercelRequest, VercelResponse } from "@vercel/node";
+import { prisma } from "@bettrdash/db";
+
+const handler = async (_request: VercelRequest, _response: VercelResponse) => {
+  let websites = await prisma.website.findMany();
+  websites.forEach(async (website) => {
+    if (isURL(website.url as string)) {
+      let url = website.url;
+      if (website.url.substring(0, 4) !== "http") {
+        url = `https://${website.url}`;
+      }
+      await axios
+        .get(url)
+        .then(async (res) => {
+          if (res.status === 200) {
+            if (website.status !== "UP") {
+              await prisma.website.update({
+                where: {
+                  id: website.id,
+                },
+                data: {
+                  status: "UP",
+                },
+              });
+            }
+          } else {
+            if (website.status !== "DOWN") {
+              await prisma.website.update({
+                where: {
+                  id: website.id,
+                },
+                data: {
+                  status: "UP",
+                },
+              });
+            }
+          }
+        })
+        .catch(async (e) => {
+          await prisma.website.update({
+            where: {
+              id: website.id,
+            },
+            data: {
+              status: "INVALID URL",
+            },
+          });
+          console.log(`${website.url} --> ${e.message}`);
+          console.log("----DOWN----");
+          return;
+        });
+    } else {
+      await prisma.website.update({
+        where: {
+          id: website.id,
+        },
+        data: {
+          status: "INVALID_URL",
+        },
+      });
+    }
+  });
+};
+
+export default handler;
+
 import express from "express";
 import cron from "cron";
 import "dotenv-safe/config";
 import axios from "axios";
 import { isURL } from "./utils/url";
-import { prisma } from "@bettrdash/db";
 
 const main = () => {
   const app = express();

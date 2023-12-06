b7e80a74b5b1e29bf012faad0ddc5806ce307589
Elias Wambugu <e.wambugu192@gmail.com>
2023-12-05 18:12:00 -0800

    added return repsonse for verecl

--- apps/monitor/api/index.ts
+++ apps/monitor/api/index.ts
@@ -1,13 +1,16 @@
 import type { VercelRequest, VercelResponse } from "@vercel/node";
 import { prisma } from "@bettrdash/db";
+import { isURL } from "./utils/url";
+import axios from 'axios';
 
-const handler = async (_request: VercelRequest, _response: VercelResponse) => {
+const handler = async (_request: VercelRequest, response: VercelResponse) => {
   let websites = await prisma.website.findMany();
   websites.forEach(async (website) => {
     if (isURL(website.url as string)) {
       let url = website.url;
       if (website.url.substring(0, 4) !== "http") {
         url = `https://${website.url}`;
+        //PROD: postgresql://HelixHEX:PkZgcBQG6p5f@ep-patient-hall-78509768-pooler.us-west-2.aws.neon.tech/main_prod?sslmode=require
 
       }
       await axios
@@ -61,97 +64,7 @@ const handler = async (_request: VercelRequest, _response: VercelResponse) => {
       });
     }
   });
+  return response.json({success: true, message: 'Checked websites'});
 };
 
 export default handler;
-
-import express from "express";
-import cron from "cron";
-import "dotenv-safe/config";
-import axios from "axios";
-import { isURL } from "./utils/url";
-
-const main = () => {
-  const app = express();
-  cron;
-  const CronJob = cron.CronJob;
-
-  // run every 5 minutes
-  try {
-    const job = new CronJob(
-      "*/5 * * * *",
-      async () => {
-        let websites = await prisma.website.findMany();
-        websites.forEach(async (website) => {
-          if (isURL(website.url as string)) {
-            let url = website.url;
-            if (website.url.substring(0, 4) !== "http") {
-              url = "https://" + website.url;
-            }
-            await axios
-              .get(url)
-              .then(async (res) => {
-                if (res.status === 200) {
-                  if (website.status !== "UP") {
-                    await prisma.website.update({
-                      where: {
-                        id: website.id,
-                      },
-                      data: {
-                        status: "UP",
-                      },
-                    });
-                  }
-                } else {
-                  if (website.status !== "DOWN") {
-                    await prisma.website.update({
-                      where: {
-                        id: website.id,
-                      },
-                      data: {
-                        status: "UP",
-                      },
-                    });
-                  }
-                }
-              })
-              .catch(async (e) => {
-                await prisma.website.update({
-                  where: {
-                    id: website.id,
-                  },
-                  data: {
-                    status: "INVALID URL",
-                  },
-                });
-                console.log(`${website.url} --> ${e.message}`);
-                console.log("----DOWN----");
-                return;
-              });
-          } else {
-            await prisma.website.update({
-              where: {
-                id: website.id,
-              },
-              data: {
-                status: "INVALID URL",
-              },
-            });
-          }
-        });
-      },
-      null,
-      true,
-      "America/Los_Angeles"
-    );
-    job.start();
-  } catch (e) {
-    console.log(e.message);
-  }
-
-  app.listen(process.env.PORT, () => {
-    console.log(`🚀 Monitor API ready at http://localhost:${process.env.PORT}`);
-  });
-};
-
-main();

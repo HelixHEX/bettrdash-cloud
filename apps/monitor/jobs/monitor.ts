25e1ee70cc464b333fa1fa66aa45e65eaa0c2873
Elias Wambugu <e.wambugu192@gmail.com>
2023-12-18 11:39:24 -0800

    udpated yarn version

---
+++ apps/monitor/jobs/monitor.ts
@@ -0,0 +1,83 @@
+import { Job, cronTrigger } from "@trigger.dev/sdk";
+import { client } from "../utils/trigger";
+
+import { prisma } from "@bettrdash/db";
+import axios from "axios";
+import { isURL } from "../utils/url";
+
+client.defineJob({
+  id: "check website status'",
+  name: "Job to check if a website is down or running",
+  version: "0.0.1",
+  trigger: cronTrigger({
+    cron: "*/5 * * * *",
+  }),
+  run: async (payload, io, ctx) => {
+    await io.logger.info("Received scheduled event", {
+      payload,
+    });
+
+    let websites = await prisma.website.findMany();
+    if (websites.length > 0) {
+      websites.forEach(async (website) => {
+        if (isURL(website.url as string)) {
+          let url = website.url;
+          if (website.url.substring(0, 4) !== "http") {
+            url = `https://${website.url}`;
+          }
+          await axios
+            .get(url)
+            .then(async (res) => {
+              if (res.status === 200) {
+                if (website.status !== "UP") {
+                  await prisma.website.update({
+                    where: {
+                      id: website.id,
+                    },
+                    data: {
+                      status: "UP",
+                    },
+                  });
+                }
+              } else {
+                if (website.status !== "DOWN") {
+                  await prisma.website.update({
+                    where: {
+                      id: website.id,
+                    },
+                    data: {
+                      status: "DOWN",
+                    },
+                  });
+                }
+              }
+            })
+            .catch(async (e) => {
+              await prisma.website.update({
+                where: {
+                  id: website.id,
+                },
+                data: {
+                  status: "INVALID URL",
+                },
+              });
+              console.log(`${website.url} --> ${e.message}`);
+              console.log("----DOWN----");
+              return;
+            });
+        } else {
+          await prisma.website.update({
+            where: {
+              id: website.id,
+            },
+            data: {
+              status: "INVALID_URL",
+            },
+          });
+        }
+      });
+    }
+
+    return { message: "Checked websites" };
+  },
+});

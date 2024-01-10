b1c887cac02e75cad0af5d630543911495f9fe67
Elias Wambugu <e.wambugu192@gmail.com>
2024-01-10 12:41:46 -0800

    removed old monitor folder

--- apps/monitor-old/app/api/test/route.ts
+++
@@ -1,8 +0,0 @@
-import { NextResponse } from "next/server"
-import { prisma } from "@bettrdash/db";
-
-export const GET = async () => {
-  const services = await prisma.service.findMany();
-  console.log(services)
-  return NextResponse.json('hi')
-}

04a93728fde530cea525defcd532fdc0b84f1c7d
Elias Wambugu <e.wambugu192@gmail.com>
2024-01-27 01:06:39 -0800

    filter websites to only show what's in the current project when adding a new website to track analytics for

--- apps/website/src/pages/analytics/Analytics.tsx
+++ apps/website/src/pages/analytics/Analytics.tsx
@@ -17,11 +17,11 @@ import {
   useColorModeValue,
   useDisclosure,
 } from "@chakra-ui/react";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { CopyBlock, dracula } from "react-code-blocks";
 import { FiTrash } from "react-icons/fi";
 import { useQuery } from "react-query";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import { analytics, useRemoveTracking, useTrackWebsite } from "../../api";
 import Loading from "../../components/Loading";
 import ModalComp from "../../components/ModalComp";
@@ -30,12 +30,18 @@ import { WebsiteProps } from "../../utils/types";
 const Analytics = () => {
   const navigate = useNavigate();
   const bg = useColorModeValue("white", "gray.800");
+  const { projectId } = useParams();
   const { data: analyticsData, status: analyticsStatus } = useQuery({
     queryKey: "analytics",
     queryFn: analytics,
     refetchInterval: 300000,
   });
 
+  useEffect(() => {
+    if (analyticsData && analyticsData.allWebsites) {
+      analyticsData.allWebsites.forEach((w:any) => console.log(w.projectId === parseInt(projectId!)))
+    }
+  }, [analyticsData]);
   if (analyticsStatus === "loading") {
     return <Loading />;
   }
@@ -49,7 +55,22 @@ const Analytics = () => {
   }
 
   const websites = analyticsData.websites;
-  const allWebsites = analyticsData.allWebsites;
+  // const allWebsites2 =
+  //   analyticsData.allWebsites.length > 0
+  //     ? analyticsData.allWesbites.filter(
+  //         (website: any) => website.projecId === projectId!
+  //       )
+  //     : analyticsData.allWebsites;
+
+  const allWebsites =
+    analyticsData &&
+    analyticsData.allWebsites &&
+    analyticsData.allWebsites.length > 0
+      ? analyticsData.allWebsites.filter(
+          (website: any) => website.projectId === parseInt(projectId!)
+        )
+      : null;
+  // const allWebsites = analyticsData.allWebsites
 
   return (
     <>
@@ -64,7 +85,7 @@ const Analytics = () => {
         p={5}
         bg={bg}
       >
-        <TrackWebsite websites={allWebsites} />
+        {allWebsites && <TrackWebsite websites={allWebsites} />}
         {websites.length > 0 ? (
           <TableContainer mt={5}>
             <Table variant="simple">
@@ -84,7 +105,9 @@ const Analytics = () => {
                         textDecoration: "underline",
                       }}
                       onClick={() =>
-                        navigate(`/projects/${website.website.projectId}/analytics/${website.website.id}`)
+                        navigate(
+                          `/projects/${website.website.projectId}/analytics/${website.website.id}`
+                        )
                       }
                     >
                       {website.website.url}
@@ -107,7 +130,9 @@ const Analytics = () => {
           </TableContainer>
         ) : (
           <Text mt={5}>
-            No websites are being tracked. Feel free to track one!
+            {!allWebsites || allWebsites.length === 0
+              ? "Please create a website first in the monitor tab"
+              : "No websites are being tracked. Feel free to track one!"}
           </Text>
         )}
       </Flex>

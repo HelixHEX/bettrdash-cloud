a6a4f4872c933f41915d0a16e37c462b3654bd19
Elias Wambugu <e.wambugu192@gmail.com>
2024-02-21 10:49:00 -0800

    cleaning up code

--- apps/auth-api/src/index.ts
+++ apps/auth-api/src/index.ts
@@ -1,5 +1,5 @@
 import "dotenv-safe/config.js";
-import express, { Application, Request, Response } from "express";
+import express from "express";
 import morgan from "morgan";
 import cors from "cors";
 
